aa60c701bd73a14cf27342e7cd52fc50f6118554
mskk0803 <toyama2ura@gmail.com>
2021-07-04 06:55:31 +0900

    FizzBuzz

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -8,13 +8,15 @@ const genkiFunction = function(number) {
   }
 }
 
-const FizzBuzz = function(num){
-  for (let x = 1; x <= num x++){
+const FizzBuzz = function(num) {
+  for (let x = 1; x <= num; x++) {
     if (x % 3 === 0) {
-      console.log(x + "FIzz")
-    }else if (x % 5 === 0 ){
-      console.log(x + "Buzz")
-    }else {
+      console.log("Fizz")
+    } else if (x % 5 === 0) {
+      console.log("Buzz")
+    } else if (x % 15 === 0) {
+      console.log("FizzBuzz")
+    } else {
       console.log(x)
     }
   }
