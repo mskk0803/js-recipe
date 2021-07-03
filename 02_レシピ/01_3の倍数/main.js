0cca7bf5f34ffc779ab34aa1010124e129b49992
mskk0803 <toyama2ura@gmail.com>
2021-07-04 06:33:48 +0900

    if式

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,7 +1,8 @@
 const number = 24
-
-if (number % 3 === 0) {
-  console.log(number + "!!!!!!!")
-} else {
-  console.log(number)
+for (let n = 1; n <= number; n++) {
+  if (number % 3 === 0) {
+    console.log(number + "!!!!!!!")
+  } else {
+    console.log(number)
+  }
 }
